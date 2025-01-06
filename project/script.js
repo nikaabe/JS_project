18443c9b9093eaacf35abc604d9f65122f9e971a
nikaabe <nikoloz.abesadze@gau.edu.ge>
2025-01-06 19:17:42 +0400

    added functionality
    functionality of slider, I managed to use my basic data structure knowledge to use it for slider productivity, and that is - circular indexing, no matter how many times u click the slider to go forward - if it passes it's row barrier it will get back to it's starting place by using module parameter. I'll talk about it more when I'll present it.

---
+++ project/script.js
@@ -0,0 +1,18 @@
+let presentIndx = 0;
+const rows = document.querySelectorAll('.book-row');
+const rowContainer = document.querySelector('.book-row-container');
+const backB = document.querySelector('.back');
+const forwardB = document.querySelector('.forward');
+
+function updateIndex(x) {
+    presentIndx = (presentIndx + x + rows.length) % rows.length;
+    presentRow(presentIndx);
+}
+
+function presentRow(i) {
+    rowContainer.style.transform = `translateX(-${i * 100}%)`;
+}
+presentRow(presentIndx);
+
+backB.addEventListener('click', () => updateIndex(-1));
+forwardB.addEventListener('click', () => updateIndex(1));
